aeab501407b23e6fad0b1c57d9c8b113f7f30e07
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-25 19:32:13 +0545

    Update server

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"go_todoApp/handlers"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -11,13 +13,28 @@ import (
 func main() {
 
 	router := mux.NewRouter()
+	// router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("/static/"))))
+	router.HandleFunc("/static/{rest:.*}", serveStatic)
 	router.HandleFunc("/", serverIndex)
 
 	router.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
 	router.HandleFunc("/tasks", handlers.CreateTask).Methods("POST")
+	router.HandleFunc("/delete", handlers.DeleteTask).Methods("POST")
+	router.HandleFunc("/update", handlers.UpdateTask).Methods("POST")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
 func serverIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
+}
+
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	fmt.Println("Trying to serve:", path)
+	if strings.HasSuffix(path, ".css") {
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	} else {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	}
+	http.ServeFile(w, r, path)
 }
