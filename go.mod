ac9e24d8ededb38098512d5fee0da37e0d711454
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-04 13:53:21 +0545

    Update

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module go_todoApp
 
 go 1.21.3
-
-require (
-	github.com/go-sql-driver/mysql v1.7.1
-	github.com/gorilla/mux v1.8.0
-)
--- main.go
+++ main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"go_todoApp/handlers"
-	"log"
-	"net/http"
-	"strings"
-
-	"github.com/gorilla/mux"
-)
+import "fmt"
 
 func main() {
-
-	router := mux.NewRouter()
-	router.HandleFunc("/static/{rest:.*}", serveStatic)
-	router.HandleFunc("/", serverIndex)
-
-	router.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
-	router.HandleFunc("/addTasks", handlers.CreateTask).Methods("POST")
-	router.HandleFunc("/delete", handlers.DeleteTask).Methods("POST")
-	router.HandleFunc("/update", handlers.UpdateTask).Methods("POST")
-	log.Fatal(http.ListenAndServe(":5000", router))
-}
-
-func serverIndex(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
-
-func serveStatic(w http.ResponseWriter, r *http.Request) {
-	path := "./" + r.URL.Path
-	fmt.Println("Trying to serve:", path)
-	if strings.HasSuffix(path, ".css") {
-		w.Header().Set("Content-Type", "text/css; charset=utf-8")
-	} else {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	}
-	http.ServeFile(w, r, path)
+	fmt.Println("Hello world!")
 }
