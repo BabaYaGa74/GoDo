d8378a16e9c7dd0cfae58c64e6f6b73f0b27c186
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-06 12:11:50 +0545

    Update main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -13,6 +13,9 @@ const (
 
 func main() {
 	add := flag.Bool("add", false, "Add a new todo")
+	done := flag.Int("done", 0, "Marks the todo as complete")
+	del := flag.Int("del", 0, "Deletes the todo")
+	list := flag.Bool("list", false, "Lists all todos")
 	flag.Parse()
 
 	todos := &todo.Todos{}
@@ -30,6 +33,32 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+
+	case *done > 0:
+		err := todos.MarkComplete(*done)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.StoreTodo(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *del > 0:
+		err := todos.DeleteTodo(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.StoreTodo(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *list:
 
 	default:
 		fmt.Fprintln(os.Stdout, "Invalid command!")
