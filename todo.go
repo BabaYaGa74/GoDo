d906085cd05d20242b9b98aa03984f0f8855079c
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-07 08:59:12 +0545

    Update todo

--- todo.go
+++ todo.go
@@ -3,8 +3,11 @@ package todo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -50,6 +53,10 @@ func (list *Todos) DeleteTodo(index int) error {
 	return nil
 }
 
+func (list *Todos) DeleteAll() {
+	*list = make(Todos, 0)
+}
+
 func (list *Todos) Load(filename string) error {
 	file, err := os.ReadFile(filename)
 	if err != nil {
@@ -79,5 +86,66 @@ func (list *Todos) StoreTodo(filename string) error {
 }
 
 func (t *Todos) Print() {
+	table := simpletable.New()
 
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "CreatedAt"},
+			{Align: simpletable.AlignRight, Text: "CompletedAt"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
+	for idx, item := range *t {
+		idx++
+		task := blue(item.Task)
+		done := blue("no")
+		if item.Completed {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task))
+			done = green("yes")
+		}
+		cells = append(cells, []*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", idx)},
+			{Text: task},
+			{Text: done},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.UpdatedAt.Format(time.RFC822)},
+		})
+	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: t.CountPending()},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+
+	table.Println()
+}
+
+func (list *Todos) CountPending() string {
+	pending := 0
+
+	for _, item := range *list {
+		if !item.Completed {
+			pending++
+		}
+	}
+	if list.Is_empty() {
+		return gray(fmt.Sprint("Nothing To do!"))
+	}
+
+	if pending == 0 {
+		return green(fmt.Sprint("No todos available!"))
+	}
+	return red(fmt.Sprintf("You have %d pending things to do", pending))
+
+}
+
+func (list *Todos) Is_empty() bool {
+	return len(*list) == 0
 }
